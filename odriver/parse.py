3db8cde50fb78f9f41f1d1668b7acc4ef427e4d5
Polishdudealan <atondryk@umich.edu>
2020-09-16 19:57:48 -0400

    Got rid of some print statements

--- odriver/parse.py
+++ odriver/parse.py
@@ -4,16 +4,13 @@ import csv
 with open('current_draw.txt', "r") as f:
 	data=f.read()
 	split_data = re.split('time: | current: ', data)
-	for x in range(10):
-		print(split_data[x])
+
 	with open('current_draw.csv', "w", newline='') as out_csv:
 		writer = csv.writer(out_csv)
 		writer.writerow(["time", "current"])
-
 		i = 1
 		while (i > 0 and i < (len(split_data) - 1)):
 			writer.writerow([split_data[i], split_data[i + 1]])
-			print([split_data[i], split_data[i + 1]])
 			i += 2
 
 	
