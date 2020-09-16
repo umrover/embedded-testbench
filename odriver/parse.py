50fc000b035d02e5070e5793263fe4d4c71ecd80
Polishdudealan <atondryk@umich.edu>
2020-09-16 19:56:13 -0400

    Fixed parsing issues for motor tests

--- odriver/parse.py
+++ odriver/parse.py
@@ -3,14 +3,18 @@ import csv
 
 with open('current_draw.txt', "r") as f:
 	data=f.read()
-	split_data = re.split('time: |current: ', data)
-
+	split_data = re.split('time: | current: ', data)
+	for x in range(10):
+		print(split_data[x])
 	with open('current_draw.csv', "w", newline='') as out_csv:
 		writer = csv.writer(out_csv)
-		writer.writerow(["time", "current_draw"])
+		writer.writerow(["time", "current"])
 
-		for i in range(0, len(split_data) - 1):
+		i = 1
+		while (i > 0 and i < (len(split_data) - 1)):
 			writer.writerow([split_data[i], split_data[i + 1]])
+			print([split_data[i], split_data[i + 1]])
+			i += 2
 
 	
 
