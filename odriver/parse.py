7bfb4cda9f75f0b55878bde91c42d755f2cfcea6
Carmen Giger <cgiger@umich.edu>
2020-09-16 22:49:35 -0400

    code clean up

--- odriver/parse.py
+++ odriver/parse.py
@@ -8,10 +8,8 @@ with open('current_draw.txt', "r") as f:
 	with open('current_draw.csv', "w", newline='') as out_csv:
 		writer = csv.writer(out_csv)
 		writer.writerow(["time", "current"])
-		i = 1
-		while (i > 0 and i < (len(split_data) - 1)):
+		for i in range(1, len(split_data) - 1, 2):
 			writer.writerow([split_data[i], split_data[i + 1]])
-			i += 2
 
 	
 
