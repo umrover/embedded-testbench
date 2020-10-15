5c78c12920e150e47a2de76f8c8dcf7f72d18ac8
tabiosg <71603173+tabiosg@users.noreply.github.com>
2020-10-14 20:44:49 -0400

    Update rgbreadings.py

--- rgbreadings.py
+++ rgbreadings.py
@@ -16,20 +16,20 @@ G_LS = 0x18
 B_LS = 0x1A
 
 # Powers the TCS34725 device on
+# "When a start condition is detected on the I2C bus, the device transitions to the Idle state where it checks the
+# Enable Register (0x00) PON bit."
 bus.write_byte_data(I2C_ADDRESS, 0x00, 0x03)
 
 # Sets the RGBC Gain value to 1xgain
 bus.write_byte_data(I2C_ADDRESS, 0x0F, 0x00)
 
-# Sets command register
-
 # Prints data and saves to a file
 File_object = open(r"rgb_data.txt","w")
 while(True):
     # TODO
-    rdecimal = read_word_data(I2C_ADDRESS, R_LS | 0xA6)
-    gdecimal = read_word_data(I2C_ADDRESS, G_LS | 0xA6)
-    bdecimal = read_word_data(I2C_ADDRESS, B_LS | 0xA6)
+    rdecimal = bus.read_word_data(I2C_ADDRESS, R_LS | 0xA6)
+    gdecimal = bus.read_word_data(I2C_ADDRESS, G_LS | 0xA6)
+    bdecimal = bus.read_word_data(I2C_ADDRESS, B_LS | 0xA6)
     print("R: " + str(rdecimal) +
           " G: " + str(gdecimal) +
           " B: " + str(bdecimal))
