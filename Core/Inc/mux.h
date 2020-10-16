22feb6cf487f1438c8479c71785c3a8c5db5c9ea
Carmen Giger <cgiger@umich.edu>
2020-10-16 09:47:31 -0400

    functions added to mux.h file

--- Core/Inc/mux.h
+++ Core/Inc/mux.h
@@ -6,7 +6,6 @@
 
 
 const int I2C_MUX_ADDRESS = 0x70;
-const int MUX_READ_WRITE_REG = 0xCC;  # filler address
+const int MUX_READ_WRITE_REG = 0xCC;  // filler address
 const int DEVICE_SLAVE_ADDRESS = 0x49;
-// ADDRESSES = {'t': 0x8, 'b': 0x2, 'y': 0x4, 'w': 0x1}
 int ADDRESSES[3] = {0x1, 0x2, 0x3};
