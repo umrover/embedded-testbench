5c45983bae9b1438605d1f1b38a44feb14a46e2b
Carmen <cgiger00@gmail.com>
2020-10-26 18:18:06 -0400

    Update accel.py
    make tutorial in general less hand holding

--- beaglebone_tutorial/accel.py
+++ beaglebone_tutorial/accel.py
@@ -7,28 +7,28 @@ from math import ceil
 bus = smbus.SMBus(2)
 
 # Accelerometer I2C address.
-I2C_DEV_ADDRESS = 0x53
+# TODO: find this in the datasheet
+I2C_DEV_ADDRESS = <insert address val here> 
 
 # Relevant registers.
-X_LS = 0x32
-X_MS = 0x33
-Y_LS = 0x34
-Y_MS = 0x35
-Z_LS = 0x36
-Z_MS = 0x37
+X_LSB = 0x32
+X_MSB = 0x33
+Y_LSB = 0x34
+Y_MSB = 0x35
+Z_LSB = 0x36
+Z_MSB = 0x37
 
-POWER_CTRL = 0x2D
-DATA_FORMAT = 0x31
+POWER_CTRL_REG = 0x2D
+DATA_FORMAT_REG = 0x31
 
 
 # Wakes the accelerometer from sleep mode.
-# TODO write the value to wake the accelerometer from sleep mode 
-bus.write_byte_data(I2C_DEV_ADDRESS, POWER_CTRL, <value>)
+# TODO write the value to wake the accelerometer from sleep mode
+bus.write_byte_data(<insert i2c address>, <insert register to write to>, <insert value to set register to>)
 
 # Formats output data (pg 24/25 on data sheet)
 # TODO format data so that the output range is full resolution +/- 16g 
-bus.write_byte_data(I2C_DEV_ADDRESS, DATA_FORMAT, <value>)
-
+bus.write_byte_data(<insert i2c address>, <insert register to write to>, <insert value to set register to>)
 
 # Reads 1 byte of data from a specified register on the accelerometer.
 # You will need to use this to grab both the most significant bit and
@@ -45,12 +45,13 @@ def read_data(register):
 # Uses read_data() function to get data and numpy to combine ls and ms.
 def get_decimal(lsb_register, msb_register):
     # TODO
-    # uncomment this code 
+    # uncomment and finish this code 
     # data = <combined MSB and LSB data>
-    return data * <scaled values> 
+    return data * <scalers> 
     pass
 
 # Rounds the number to a specified number of decimal places
+# usage: float_round(<number>, <number of decimal places>)
 def float_round(num, places = 0, direction = ceil):
     return direction(num * (10**places)) / float(10**places)
 
@@ -58,7 +59,8 @@ def float_round(num, places = 0, direction = ceil):
 # Write your data output loop here. It should print x, y, and z data.
 # Make sure to use your get_decimal function.
 while(True):
-    # TODO
+    # TODO: format this to print data correctly, and add the releveant functions/logic to 
+    # get the x, y, z values 
     print("X: " + str(""" x accel value""") + 
           " Y: " + str("""y accel value""") + 
           " Z: " + str("""z accel value""")) 
