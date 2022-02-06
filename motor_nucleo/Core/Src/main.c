1ac54352ff4e11497a0e66cded29bb11bd7a4ce5
tabiosg <gutstabios@gmail.com>
2022-02-06 17:02:46 -0500

    Change how multiplier is determined

--- motor_nucleo/Core/Src/main.c
+++ motor_nucleo/Core/Src/main.c
@@ -17,7 +17,6 @@
   ******************************************************************************
   */
 /* USER CODE END Header */
-
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
@@ -110,13 +109,13 @@ float absEncFilter(int channel, float raw_val)
     return raw_val;
   }
 
-  float multiplier = 1;
+  float multiplier = STABILIZER_MULTIPLIER;
   
   if (fabs(raw_val - (channels + channel)->abs_enc_value) > ENCODER_ERROR_THRESHOLD) {
     multiplier = STABILIZER_BAD_MULTIPLIER;
   }
-  else {
-    multiplier = STABILIZER_MULTIPLIER;
+  else if (fabs(raw_val) > 6.25) {
+    multiplier = 1;
   }
   
   return multiplier * (channels + channel)->abs_enc_value + (1 - multiplier) * raw_val;
@@ -318,7 +317,8 @@ void SystemClock_Config(void)
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the CPU, AHB and APB busses clocks
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
@@ -328,7 +328,7 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  /** Initializes the CPU, AHB and APB busses clocks
+  /** Initializes the CPU, AHB and APB buses clocks
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
@@ -845,7 +845,7 @@ void Error_Handler(void)
   * @param  line: assert_param error line source number
   * @retval None
   */
-void assert_failed(char *file, uint32_t line)
+void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
