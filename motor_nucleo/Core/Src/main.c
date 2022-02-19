709df269ec4ef2ab298788e16637952281dbb216
Carmen Giger <cgiger00@gmail.com>
2022-02-19 17:46:03 -0500

    increased PWM frequency to 20 kHz in channels 0,1, and 2

--- motor_nucleo/Core/Src/main.c
+++ motor_nucleo/Core/Src/main.c
@@ -476,9 +476,9 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 7;
+  htim1.Init.Prescaler = 3;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 1000;
+  htim1.Init.Period = 100;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
