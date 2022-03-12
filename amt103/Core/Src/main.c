af22661ce517edf24524a4cba281f2e6c9a4d296
tabiosg <gutstabios@gmail.com>
2022-03-12 12:52:55 -0500

    Add intial testing code

---
+++ amt103/Core/Src/main.c
@@ -0,0 +1,310 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdio.h"  // for sprintf
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim3;
+
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM3_Init(void);
+static void MX_USART1_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM3_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_ALL);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  uint16_t quad_enc_value = 0;
+  uint16_t quad_enc_raw_last = 0;
+  uint16_t quad_enc_raw_now = 0;
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  /*
+	   * PA6 is M1 QUAD A
+	   * PA4 is M1 QUAD B
+	   * PB8 is J I2C SCL
+	   * PB9 is J I2C SDA
+	   * Datasheet: https://www.cuidevices.com/product/resource/amt10.pdf
+	   * Device is AMT103 2048 N4000.
+	   *
+	   */
+		quad_enc_raw_now = TIM3->CNT;
+		quad_enc_value = (int16_t)(quad_enc_raw_now - quad_enc_raw_last) + quad_enc_value;
+		quad_enc_raw_last = quad_enc_raw_now;
+
+		char string[50] = "";
+
+		sprintf((char *)string, "Quad val is %u, quad nowis %u, quad last is%u,\r\n", quad_enc_value, quad_enc_raw_now, quad_enc_raw_last);
+		HAL_Delay(100);
+		HAL_UART_Transmit_IT(&huart1, (uint8_t *)string, sizeof(string));
+		HAL_Delay(15);
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_TIM34;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
+  PeriphClkInit.Tim34ClockSelection = RCC_TIM34CLK_HCLK;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_Encoder_InitTypeDef sConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 0;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 65535;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  sConfig.EncoderMode = TIM_ENCODERMODE_TI1;
+  sConfig.IC1Polarity = TIM_ICPOLARITY_RISING;
+  sConfig.IC1Selection = TIM_ICSELECTION_DIRECTTI;
+  sConfig.IC1Prescaler = TIM_ICPSC_DIV1;
+  sConfig.IC1Filter = QUADRATURE_FILTER;
+  sConfig.IC2Polarity = TIM_ICPOLARITY_RISING;
+  sConfig.IC2Selection = TIM_ICSELECTION_DIRECTTI;
+  sConfig.IC2Prescaler = TIM_ICPSC_DIV1;
+  sConfig.IC2Filter = QUADRATURE_FILTER;
+  if (HAL_TIM_Encoder_Init(&htim3, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 38400;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
---
+++ motor_nucleo/README.md
@@ -0,0 +1,128 @@
+# MOTOR NUCLEO FIRMWARE
+## Table of Contents
+[Overview](#Overview) \
+[Registers](#Registers) \
+[Commands](#Commands) \
+[IO](#IO)
+
+## Overview
+This firmware configures one nucleo f303re to act as a "motor nucleo".
+
+Motor nucleos meet their functionality by internally splitting themselves into 6 distinct controller units, termed “channels”, labeled 0, 1, 2, 3, 4, and 5. **Each channel controls one motor independently**.
+
+Each channel’s interfaces are exposed on the IO of the Nucleo F303RE. **Each channel’s interfaces have different IO. Some interfaces use shared IO**
+
+Motor nucleos can control up to 6 motors, with each motor assigned to a "channel"
+- All channels **share** an I2C bus to receive commands from a master device
+- All channels **own** one PWM output (0%-100% duty cycle = throttle of motor)
+- All channels **own** one DIR and NDIR output (logic HIGH/LOW = direction of motor, NDIR = !DIR)
+- All channels **own** one LIMIT SWITCH input (logic HIGH = stop PWM)
+- Channels 0, 1, 2 **own** one set of QUAD ENCODER A and QUAD ENCODER B inputs
+- Channels 0, 1, 2 **share** an I2C bus to command ABSOLUTE ENCODERS
+
+## Registers
+Each channel has its own set of registers that dictate the channel’s behavior. **These registers are volatile (will not be saved under loss of power)**
+
+| Register | Data Type | Value | Reset Value | Units |
+| -------- | --------- | ----- | ----------- | ----- |
+| control_mode | hidden | OPEN/CLOSED -> open/closed loop control is used. Set to OPEN upon OPEN or OPEN_PLUS command. Set to CLOSED upon CLOSED or CLOSED_PLUS command. | OPEN |
+| dir | 8-bit value | 1 -> DIR/NDIR = HIGH/LOW, 0 -> DIR/NDIR = LOW/HIGH | 1 |
+| open_setpoint | 16-bit unsigned integer | This register is an input for open-loop control | 0 | 0.125 us |
+| closed_setpoint | 32-bit float | This register is an input for closed-loop control | 0 | quadrature encoder counts |
+| closed_FF | 32-bit float | This register is an input for closed-loop control | 0 | |
+| closed_KP | 32-bit float | This register is an input for closed-loop control | 0 | |
+| closed_KI | 32-bit float | This register is an input for closed-loop control | 0 | |
+| closed_KD | 32-bit float | This register is an input for closed-loop control | 0 | |
+| quad_enc | 32-bit integer | This register is automatically incremented/decremented upon quadrature encoder feedback | 0 | quadrature encoder counts |
+| abs_enc | 32-bit float | This register is a passthrough to the absolute encoder's position register. Reading from this register is equivalent to reading from an absolute encoder. | 0 | absolute encoder counts |
+| pwm_max | 16-bit unsigned integer | VALUE = maximum PWM pulse width. Use this register to protect lower voltage motors. | 0 | 0.125 uS |
+
+In closed-loop control mode, the Nucleo will use output from a standard PID loop. The error for the PID loop is calculated by *closed_setpoint – quad_enc*. The PWM pulse width and PWM direction will automatically be calculated, with a maximum pulse width of pwmMax.
+
+In open-loop control mode, the Nucleo will output *open_setpoint* as the PWM pulse width, *dir* for DIR, and the ! *dir* for NDIR. 
+
+
+
+## Commands
+Each channel behaves as an I2C slave. The first three bits of the channel’s address correspond to the motor nucleo's designated number (plus 1) on the rover, while the last four bits of the address correspond to the channel’s number on the motor nucleo.
+
+> For example, channel 2 on Nucleo 3 would have the I2C address of 100 0010.
+
+The Nucleo expects transactions to adhere to the following format. For each transaction, the master should follow these steps:
+1.	Issue a START condition
+2.	Send the channel’s 7-bit address followed by a Write bit
+3.	Send one byte to indicate the desired command (see "Command")
+4.	Send the appropriate number of data bytes (see "Host Data"). (Could be 0)
+5.  If no nucleo data is to be received, issue a STOP condition. End of transaction.
+6.	Else, issue a RESTART condition, OR issue a STOP condition and then a START condition
+7.	Send the channel’s 7-bit address followed by a Read bit
+8.	Accept the appropriate number of data bytes (see "Nucleo Data")
+9.	Issue a NACK
+10.	Issue a STOP condition. End of transaction.
+
+> These transactions are not based off of any smbus standard. It is recommended to use the open(), ioctl(), read(), and write() functions defined in linux/i2c-dev.h to control the host's I2C interface.
+
+Host data sent to the channel will overwrite the appropriate registers in that channel. Nucleo data received from the channel will contain the most recent value of appropriate registers in that channel.
+
+The table below lists the expected data order / number of bytes (in parenthesis) for every command.
+>Transactions not following the proper format will be dropped in most cases.
+
+| Command | Hex Code | Host Data | Nucleo Data |
+| ------- | -------- | --------- | ----------- |
+| OPEN | 0x10 | dir(1), open_setpoint(2) | (0) | 
+| OPEN_PLUS | 0x1F | dir(1), open_setpoint(2) | quad_enc(4) | 
+| CLOSED | 0x20 | closed_setpoint(4), closed_FF(4) | (0) |
+| CLOSED_PLUS | 0x2F | closed_setpoint(4), closed_FF(4) | quad_enc(4) |
+| SET_MAX_PWM | 0x30 | pwm_max(2) | (0) |
+| SET_CLOSED_K | 0x3F | closed_KP(4), closed_KI(4), closed_KD(4) | (0) |
+| QUAD_ENC | 0x40 | (0) | quad_enc(4) |
+| QUAD_ENC_ADJUST | 0x4F | quad_enc(4) | (0) |
+| ABS_ENC | 0X50 | (0) | abs_enc(2) |
+
+>If the motor nucleo does not receive any commands from the host for a period longer than 1 second, the motor nucleo will reset all channels to open loop control, with an *open_setpoint* of 0, stopping all motors.
+
+## IO
+| Interface | Channel | IO |
+| --------- | ------- | -- |
+| HOST I2C - SCL | ALL | PB8 |
+| HOST I2C - SDA | ALL | PB9 |
+| PWM - PWM | 0 | PC0 | 
+| PWM - PWM | 1 | PC1 | 
+| PWM - PWM | 2 | PC2 | 
+| PWM - PWM | 3 | PC6 | 
+| PWM - PWM | 4 | PC7 | 
+| PWM - PWM | 5 | PC8 | 
+| PWM - DIR | 0 | PA10 | 
+| PWM - DIR | 1 | PA11 | 
+| PWM - DIR | 2 | PA12 | 
+| PWM - DIR | 3 | PA13 | 
+| PWM - DIR | 4 | PA14 | 
+| PWM - DIR | 5 | PA15 | 
+| PWM - NDIR | 0 | PC10 | 
+| PWM - NDIR | 1 | PC11 | 
+| PWM - NDIR | 2 | PC12 | 
+| PWM - NDIR | 3 | PC13 | 
+| PWM - NDIR | 4 | PC14 | 
+| PWM - NDIR | 5 | PC15 |
+| LIMIT SWITCH | 0 | PB10 | 
+| LIMIT SWITCH | 1 | PB11 | 
+| LIMIT SWITCH | 2 | PB12 | 
+| LIMIT SWITCH | 3 | PB13 | 
+| LIMIT SWITCH | 4 | PB14 | 
+| LIMIT SWITCH | 5 | PB15 |
+| ENCODER QUAD - A | 0 | PA0 |
+| ENCODER QUAD - A | 1 | PA6 |
+| ENCODER QUAD - A | 2 | PB7 |
+| ENCODER QUAD - B | 0 | PA1 |
+| ENCODER QUAD - B | 1 | PA4 |
+| ENCODER QUAD - B | 2 | PB6 |
+| ENCODER I2C - SCL | 0, 1, 2 | PF0 |
+| ENCODER I2C - SDA | 0, 1, 2 | PF1 |
+
+If any Master I2C IO is disconnected, all channels will not respond to commands. It is imperative this interface is connected.
+
+If any PWM output IO is disconnected, there will be no internal effects on the channel.
+
+If any quadrature input IO is disconnected, the affected channel will sense incorrect or no quadrature encoder input.
+
+If any encoder I2C IO is disconnected, all channels will not respond to commands. It is imperative this interface is connected.
