3a61a5b65bc734b37f675f166854b75dbf5ac520
tabiosg <gutstabios@gmail.com>
2022-09-22 20:53:30 -0400

    Add smbus library

--- science-dev/Core/Src/spectral.c
+++ science-dev/Core/Src/spectral.c
@@ -1,30 +1,24 @@
-#ifndef SPECTRAL_H_
-#define SPECTRAL_H_
-
-#include "stm32g0xx_hal.h"
-#include "stdint.h"
-#include "smbus.h"
-
-// AS7262 Spectral sensor
-typedef struct {
-	SMBus *smbus;
-} Spectral;
+#include "spectral.h"
 
 // REQUIRES: hi2c is the i2c channel
 // MODIFIES: nothing
 // EFFECTS: Returns a pointer to a created Spectral object
-Spectral *new_spectral(I2C_HandleTypeDef *_i2c);
+Spectral *new_spectral(I2C_HandleTypeDef *_i2c) {
+    SMBus *_smbus = new_smbus(_i2c);
+    Spectral *spectral = malloc(sizeof(Spectral));
+    spectral->smbus = _smbus;
+}
 
 // REQUIRES: spectral is a Spectral object
 // MODIFIES: nothing
 // EFFECTS: Initializes the spectral device
-void initialize_spectral(Spectral* spectral);
+void initialize_spectral(Spectral* spectral) {
+
+}
 
 // REQUIRES: spectral is an object and 0 <= channel < 6
 // MODIFIES: nothing
 // EFFECTS: Returns the spectral data of a particular channel
-uint32_t get_spectral_channel_data(Spectral *spectral, uint8_t channel);
+uint32_t get_spectral_channel_data(Spectral *spectral, uint8_t channel) {
 
-#endif
-
-//#endif
+}
