5b24c9dceed4a95a92441f301c12a16531760fc6
Guthrie <71603173+tabiosg@users.noreply.github.com>
2022-10-25 12:13:15 -0400

    Add some code

--- science-dev/Core/Src/spectral.c
+++ science-dev/Core/Src/spectral.c
@@ -12,9 +12,8 @@ Spectral *new_spectral(
     UART_HandleTypeDef *uart,
     bool dma)
 {
-    SMBus *_smbus = new_smbus(i2c, uart, 0x49, dma);
     Spectral *spectral = malloc(sizeof(Spectral));
-    spectral->smbus = _smbus;
+    spectral->smbus = new_smbus(i2c, uart, 0x49, dma);
     return spectral;
 }
 
@@ -35,4 +34,75 @@ void initialize_spectral(Spectral *spectral)
 uint32_t get_spectral_channel_data(Spectral *spectral, uint8_t channel)
 {
     return 0;
+}
+
+// sets enable bits in devices
+void enable_spectral(Spectral *spectral) {
+    // 0x28 means the following:
+    // RST is 0, so no reset is done
+    // INT Is 0, so no interrupt
+    // GAIN is 0b10, so it is 16x sensor channel gain
+    // BANK is 0b10, so data conversion is Mode 2
+    // DATA_RDY is 0 and RSVD is 0
+    write_spectral(spectral, CONTROL_SET_UP, 0x28);  // runs twice to account for status miss
+    HAL_Delay(5);
+    write_spectral(spectral, CONTROL_SET_UP, 0x28);  // converts data bank to 2
+    write_spectral(spectral, INT_TIME, 0xFF);  // increases integration time
+}
+
+/* PRIVATE FUNCTIONS */
+
+void write_spectral(Spectral *spectral, uint8_t v_reg, uint8_t data) {
+    // Taken from datasheet
+    uint8_t status;
+
+	while(1) {
+		status = read_byte_data(spectral->smbus, DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_STATUS_REG);
+		if ((status & I2C_AS72XX_SLAVE_TX_VALID) == 0) {
+			break;
+		}
+		HAL_Delay(5);
+	}
+	write_byte_data(spectral->smbus, DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_WRITE_REG, (v_reg | 0x80));
+
+	while(1) {
+		status = read_byte_data(spectral->smbus, DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_STATUS_REG);
+		if ((status & I2C_AS72XX_SLAVE_TX_VALID) == 0) {
+			break;
+		}
+		HAL_Delay(5);
+	}
+	write_byte_data(spectral->smbus, DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_WRITE_REG, data);
+}
+
+uint8_t _virtual_read(Spectral *spectral, uint8_t v_reg) {
+    // Taken from datasheet
+    uint8_t status;
+	uint8_t d;
+	status = read_byte_data(spectral->i2c_bus, DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_STATUS_REG);
+
+	if ((status & I2C_AS72XX_SLAVE_RX_VALID) != 0) {
+		// d = nucleo_byte_read(I2C_AS72XX_SLAVE_READ_REG);
+		d = read_byte_data(spectral->i2c_bus, DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_READ_REG);
+	}
+
+	while(1) {
+		status = read_byte_data(spectral->i2c_bus, DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_STATUS_REG);
+		if ((status & I2C_AS72XX_SLAVE_TX_VALID) == 0) {
+			break;
+		}
+		HAL_Delay(5); //delay for 5 ms
+	}
+
+	write_byte_data(spectral->i2c_bus, DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_WRITE_REG, v_reg);
+	while(1) {
+		status = read_byte_data(spectral->i2c_bus, DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_STATUS_REG);
+		if ((status & I2C_AS72XX_SLAVE_RX_VALID) != 0) {
+			break;
+		}
+		HAL_Delay(5); //delay for 5 ms
+	}
+
+	d = read_byte_data(spectral->i2c_bus, DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_READ_REG);
+	return d;
 }
