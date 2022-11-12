c2ff78b716f9b1d5d4f646e8a8dd910a6088c0ca
tabiosg <gutstabios@gmail.com>
2022-11-11 23:00:04 -0500

    Add science dev board changes

---
+++ science-dev-board/Core/Inc/diagnostic_temp_sensor.h
@@ -0,0 +1,36 @@
+#pragma once
+// Part Link
+// https://www.digikey.com/en/products/detail/microchip-technology/MCP9701T-E-TT/1987445
+
+#include "stm32f3xx_hal.h"
+
+#include "adc_sensor.h"
+#include <stdint.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+#define DIAG_TEMP_COEFFICIENT 19.5f // mV/Celsius
+#define DIAG_TEMP_ZERO_DEGREE_OUTPUT 400 //mV
+
+typedef struct {
+    uint8_t channel;
+    float temp;
+    ADCSensor* adc_sensor;
+} Temp_Sensor;
+
+// REQUIRES: _adc_channel is the corresponding ADC channel and
+// _adc_sensor is a pointer to an ADCSensor object
+// MODIFIES: nothing
+// EFFECTS: Returns a pointer to a created temp sensor object
+Temp_Sensor* new_diagnostic_temp_sensor(ADCSensor* adc_sensor, int channel);
+
+// REQUIRES: valid temp sensor
+// MODIFIES: stored sensor value
+// EFFECTS: updates the sensor value
+void update_temp_sensor_value(Temp_Sensor* sensor);
+
+// REQUIRES: valid temp sensor
+// MODIFIES: nothing
+// EFFECTS: returns the stored value for amps
+float get_temp_sensor_temp(Temp_Sensor* sensor);
