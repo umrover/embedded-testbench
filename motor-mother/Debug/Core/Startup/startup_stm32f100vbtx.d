380ba5c6578c571920a7f7991835a9f0337227c0
wisabel0 <wisabel@umich.edu>
2023-09-12 20:30:06 -0400

    cleanup

--- motor-mother/Debug/Core/Startup/startup_stm32f100vbtx.d
+++
@@ -1,2 +0,0 @@
-Core/Startup/startup_stm32f100vbtx.o: \
- ../Core/Startup/startup_stm32f100vbtx.s
--- motor_nucleo/Debug/Core/Src/closed_loop_control.d
+++
@@ -1,3 +0,0 @@
-Core/Src/closed_loop_control.o: ../Core/Src/closed_loop_control.c \
- ../Core/Inc/closed_loop_control.h
-../Core/Inc/closed_loop_control.h:
